fae0103e4335840073095804564636a2e1c90af6
Cody Hatfield <cody.hatfield@me.com>
2022-05-05 14:23:05 -0700

    Generate types and export ModelTypes

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import type { ModelTypeAliases } from "@glazed/types";
+import { BasicProfile } from "@datamodels/identity-profile-basic";
+import { MediaGallery } from "../types/MediaGallery";
+import { Pinset } from "../types/Pinset";
+
+export type ModelTypes = ModelTypeAliases<
+  {
+    BasicProfile: BasicProfile;
+    MediaGallery: MediaGallery;
+    GeoWebPinset: Pinset;
+  },
+  {
+    basicProfile: "BasicProfile";
+    mediaGallery: "MediaGallery";
+    geoWebPinset: "GeoWebPinset";
+  }
+>;
+
+export const model = require("./model.json");
