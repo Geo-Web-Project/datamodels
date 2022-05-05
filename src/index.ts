46e331bc2f94407d47fdcee95bc0cd9a4c9b469a
Cody Hatfield <cody.hatfield@me.com>
2022-05-05 14:25:47 -0700

    Export types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { BasicProfile } from "@datamodels/identity-profile-basic";
 import { MediaGallery } from "../types/MediaGallery";
 import { Pinset } from "../types/Pinset";
 
+export type { BasicProfile } from "@datamodels/identity-profile-basic";
+export type { MediaGallery } from "../types/MediaGallery";
+export type { Pinset } from "../types/Pinset";
+
 export type ModelTypes = ModelTypeAliases<
   {
     BasicProfile: BasicProfile;
